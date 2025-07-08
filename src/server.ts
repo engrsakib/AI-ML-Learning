96951b69a9432f21eeef1d7ef7fcd49f20026f1f
engrsakib <snazmussakib01@gmail.com>
2025-07-08 20:52:14 +0600

    server is finaly running

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,23 @@
 import { Server } from "http";
+import express, { Express } from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
 
-const server : Server;
+dotenv.config();
+
+let server : Server;
+const app: Express = express();
+
+
+const startServer = async(port: number): Promise<void> => {
+    try {
+        await mongoose.connect(process.env.DATABASE_URL as string);
+        server = app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
+    } catch (error: unknown) {
+        console.error('Error connecting to MongoDB:', error);
+    }
+}
+
+startServer(Number(process.env.PORT) || 1000);
