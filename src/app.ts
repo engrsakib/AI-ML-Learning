bd8af47623c00f85ab755e5798c33bec384279d5
engrsakib <snazmussakib01@gmail.com>
2025-07-11 01:06:45 +0600

    routes update a dynamic way

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 import express, { Express, Request, Response } from "express";
-
+import cors from "cors";
+import { router } from "./app/routes";
 const app: Express = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
+app.use("/api/v1", router);
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json("Welcome to the Tour Management System");
---
+++ src/app/routes/index.ts
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import { UserRoutes } from "../modules/user/user.routes";
+
+export const router = Router();
+
+
+
+const moduleRoutes = [
+  {
+    path: "/users",
+    route: UserRoutes,
+  },
+];
+
+moduleRoutes.forEach((route) => {
+  router.use(route.path, route.route);
+
+
+});
