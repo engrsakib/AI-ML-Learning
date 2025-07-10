39b42f554709d68f908e1ba9d85b6718ca74cab2
engrsakib <snazmussakib01@gmail.com>
2025-07-11 04:34:11 +0600

    create user ready
    we make sure our create user is fully funconalize but at this time we don't implement oure password hsaing system

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,11 +1,11 @@
 import { Request, Response } from "express";
 import httpStatus from "http-status-codes";
 import { User } from "./user.model";
+import { UserService } from "./user.service";
 
 const createUser = async (req: Request, res: Response) => {
   try {
-    const user = req.body;
-    const newUser = await User.create(user);
+    const newUser = await UserService.createUser(req.body);
     res.status(httpStatus.CREATED).json({
       message: "User created successfully",
       user: newUser,
---
+++ src/app/modules/user/user.service.ts
@@ -0,0 +1,12 @@
+import { IUser } from "./user.interface";
+import { User } from "./user.model";
+
+const createUser = async (payload: Partial<IUser>) => {
+  const user = new User(payload);
+  const newUser = await User.create(user);
+  return newUser;
+};
+
+export const UserService = {
+  createUser,
+};
