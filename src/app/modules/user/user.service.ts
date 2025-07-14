ceba3342d80873ee9579c5b5e5b2f7895feb35bb
engrsakib <snazmussakib01@gmail.com>
2025-07-15 01:47:01 +0600

    npm i bcrypt setup
    we usaly use it for password hassing

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -2,6 +2,14 @@ import { IUser } from "./user.interface";
 import { User } from "./user.model";
 
 const createUser = async (payload: Partial<IUser>) => {
+  if (!payload.email || !payload.password) {
+    throw new Error("Email and password are required to create a user");
+  }
+  const existingUser = await User.findOne({ email: payload.email });
+  if (existingUser) {
+    throw new Error("User with this email already exists");
+  }
+
   const user = new User(payload);
   const newUser = await User.create(user);
   return newUser;
