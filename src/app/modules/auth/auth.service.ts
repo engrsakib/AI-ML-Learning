b98811b04e7555ce9e50a8ffd0c656fa39f56b24
engrsakib <snazmussakib01@gmail.com>
2025-07-15 19:51:30 +0600

    accress update

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -2,6 +2,7 @@ import AppError from "../../errorHelpers/appError";
 import { IUser } from "../user/user.interface";
 import { User } from "../user/user.model";
 import  bcrypt  from "bcryptjs";
+import jwt from "jsonwebtoken";
 const credentialsLogin = async (payload: Partial<IUser>) => {
   const { email, password } = payload;
   // Logic for user authentication goes here
@@ -15,11 +16,20 @@ const credentialsLogin = async (payload: Partial<IUser>) => {
     throw new AppError("Invalid password", 401);
   }
 
+  const acccessToken = jwt.sign({
+    id: isUserExists._id,
+    email: isUserExists.email,
+    role: isUserExists.role,
+    name: isUserExists.name,
+  }, process.env.JWT_SECRET as string, {
+    expiresIn: "1h", // Token expiration time
+  });
+
   // If authentication is successful, return user information or token
   return {
     id: isUserExists._id,
     email: isUserExists.email,
-    // Include any other user information you want to return
+    acccessToken,
   };
 };
 
