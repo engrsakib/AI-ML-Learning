7e1cb06430d03a52d2e60b6c46664fce30cf6d34
engrsakib <snazmussakib01@gmail.com>
2025-07-15 10:12:42 +0600

    user login succes

--- src/app/modules/auth/auth.controllers.ts
+++ src/app/modules/auth/auth.controllers.ts
@@ -1,13 +1,16 @@
 import { Request, Response } from "express";
 import httpStatus from "http-status-codes";
 import { sendResponse } from "../../util/sendResponse";
+import { AuthService } from "./auth.service";
 
 const credentialsLogin = async (req: Request, res: Response) => {
+
+  const loginInfo = await AuthService.credentialsLogin(req.body);
   sendResponse(res, {
     success: true,
     message: "Login successful",
     status: httpStatus.OK,
-    data: { user, token },
+    data: { user: loginInfo },
   });
 };
 
--- src/app/modules/auth/auth.routes.ts
+++ src/app/modules/auth/auth.routes.ts
@@ -1,4 +1,7 @@
 import { Router } from "express";
+import { AuthController } from "./auth.controllers";
+
 
 const router = Router();
+router.post("/login", AuthController.credentialsLogin);
 export const AuthRoutes = router;
