360bcb713c05a231b2039f6624e13c3644e36e6a
engrsakib <snazmussakib01@gmail.com>
2025-07-15 20:30:49 +0600

    admin check error resolved

--- src/app/util/verifyAdminToken.ts
+++ src/app/util/verifyAdminToken.ts
@@ -1,7 +1,11 @@
 import { NextFunction, Request, Response } from "express";
-import  jwt  from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
-export const verifyAdminToken = (req: Request, res: Response, next: NextFunction) => {
+export const verifyAdminToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
     const acccessToken = req.headers.authorization;
     if (!acccessToken) {
@@ -10,12 +14,18 @@ export const verifyAdminToken = (req: Request, res: Response, next: NextFunction
     }
     const verifiedToken = jwt.verify(
       acccessToken,
-      process.env.JWT_SECRET as string,
+      process.env.JWT_SECRET as string
     );
     if (!verifiedToken) {
       res.status(401).json({ message: "Invalid token" });
       return;
     }
+    // Optionally, you can check if the user is an admin
+    if ((verifiedToken as jwt.JwtPayload).role !== "admin" && (verifiedToken as jwt.JwtPayload).role !== "superAdmin") {
+      res.status(403).json({ message: "You do not have permission to access this resource" });
+      return;
+    }
+
     next();
   } catch (error) {
     res.status(401).json({ message: "Unauthorized access " + error });
