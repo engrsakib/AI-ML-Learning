3bff194918910caa5a846dc887b70c14ce8c184b
engrsakib <snazmussakib01@gmail.com>
2025-07-15 20:09:22 +0600

    web token full funconalized

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -1,12 +1,41 @@
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import { UserController } from "./user.controller";
 import { createUserZodSchema } from "./user.validations";
 import { validateRequest } from "../../middleware/validateRequest";
+import jwt  from "jsonwebtoken";
 
 const router = Router();
 
-router.post("/register", validateRequest(createUserZodSchema), UserController.createUser);
-router.get("/", UserController.getAllUsers);
+router.post(
+  "/register",
+  validateRequest(createUserZodSchema),
+  UserController.createUser,
+);
+router.get(
+  "/",
+  (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const acccessToken = req.headers.authorization;
+      if (!acccessToken) {
+        res.status(401).json({ message: "Unauthorized access" });
+        return;
+      }
+      const verifiedToken = jwt.verify(
+        acccessToken,
+        process.env.JWT_SECRET as string,
+      );
+      if (!verifiedToken) {
+        res.status(401).json({ message: "Invalid token" });
+        return;
+      }
+      next();
+    
+    } catch (error) {
+      res.status(401).json({ message: "Unauthorized access " + error });
+      return;
+    }
+  },
+  UserController.getAllUsers,
+);
 
-
-export const UserRoutes = router; 
+export const UserRoutes = router;
