2ec566ab19ea0eb4aa6079ed8c0df2164e659257
engrsakib <snazmussakib01@gmail.com>
2025-07-18 02:43:46 +0600

    refresh token varify update and security update

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,8 +1,7 @@
-
 import AppError from "../../errorHelpers/appError";
 import { isActive, IUser } from "../user/user.interface";
 import { User } from "../user/user.model";
-import  bcrypt  from "bcryptjs";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 const credentialsLogin = async (payload: Partial<IUser>) => {
   const { email, password } = payload;
@@ -12,27 +11,38 @@ const credentialsLogin = async (payload: Partial<IUser>) => {
     throw new AppError("User not found", 404);
   }
 
-  const isPasswordValid = await bcrypt.compare(password as string, isUserExists.password as string);
+  const isPasswordValid = await bcrypt.compare(
+    password as string,
+    isUserExists.password as string,
+  );
   if (!isPasswordValid) {
     throw new AppError("Invalid password", 401);
   }
 
-  const acccessToken = jwt.sign({
-    id: isUserExists._id,
-    email: isUserExists.email,
-    role: isUserExists.role,
-    name: isUserExists.name,
-  }, process.env.JWT_SECRET as string, {
-    expiresIn:  Number(process.env.ACCESS_TOKEN_EXPIRES_IN) || 3600, // Default to 1 hour if not specified
-  });
-  const refreshToken = jwt.sign({
-    id: isUserExists._id,
-    email: isUserExists.email,
-    role: isUserExists.role,
-    name: isUserExists.name,
-  }, process.env.JWT_REFRESH_TOKEN_SECRET as string, {
-    expiresIn: Number(process.env.REFRESH_TOKEN_EXPIRES_IN) || 604800, // Default to 7 days if not specified
-  });
+  const acccessToken = jwt.sign(
+    {
+      id: isUserExists._id,
+      email: isUserExists.email,
+      role: isUserExists.role,
+      name: isUserExists.name,
+    },
+    process.env.JWT_SECRET as string,
+    {
+      expiresIn: Number(process.env.ACCESS_TOKEN_EXPIRES_IN) || 3600, // Default to 1 hour if not specified
+    },
+  );
+  const refreshToken = jwt.sign(
+    {
+      id: isUserExists._id,
+      email: isUserExists.email,
+      role: isUserExists.role,
+      name: isUserExists.name,
+    },
+    process.env.JWT_REFRESH_TOKEN_SECRET as string,
+    {
+      expiresIn: Number(process.env.REFRESH_TOKEN_EXPIRES_IN) || 604800, // Default to 7 days if not specified
+    },
+  );
 
   // delete isUserExists.password; // Remove password from the user object before returning
   isUserExists.password = ""; // Ensure password is not returned in the response
@@ -53,35 +63,41 @@ const getNewAccessToken = async (refreshToken: string) => {
 
   try {
     // Verify the refresh token
-    const decoded = jwt.verify(refreshToken, process.env.JWT_REFRESH_TOKEN_SECRET as string) as IUser;
+    const decoded = jwt.verify(
+      refreshToken,
+      process.env.JWT_REFRESH_TOKEN_SECRET as string,
+    ) as IUser;
     const user = await User.findOne({ email: decoded.email });
 
     if (!user) {
       throw new AppError("User not found", 404);
     }
-    if(user){
+    if (user) {
       // Check if the user is active or not
       if (user.isActive === isActive.BLOCKED) {
         throw new AppError("User is not active", 403);
-      }else if (user.isDeleted) {
+      } else if (user.isDeleted) {
         throw new AppError("User is deleted", 403);
       }
     }
-    
 
-    const newAccessToken = jwt.sign({
-      id: user._id,
-      email: user.email,
-      role: user.role,
-      name: user.name,
-    }, process.env.JWT_SECRET as string, {
-      expiresIn: Number(process.env.ACCESS_TOKEN_EXPIRES_IN) || 3600, // Default to 1 hour if not specified
-    });
+    const newAccessToken = jwt.sign(
+      {
+        id: user._id,
+        email: user.email,
+        role: user.role,
+        name: user.name,
+      },
+      process.env.JWT_SECRET as string,
+      {
+        expiresIn: Number(process.env.ACCESS_TOKEN_EXPIRES_IN) || 3600, // Default to 1 hour if not specified
+      },
+    );
 
     // Return the new access token and user information
     user.password = ""; // Ensure password is not returned in the response
 
-    return { user, accessToken: newAccessToken };
+    return { user: user, accessToken: newAccessToken, refreshToken: refreshToken };
   } catch (error) {
     throw new AppError(`Invalid refresh token: ${error}`, 401);
   }
