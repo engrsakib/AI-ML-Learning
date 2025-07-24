13596fa63fdc81d981f416e819360ada55d4fe5b
engrsakib <snazmussakib01@gmail.com>
2025-07-24 21:17:47 +0600

    slug create logic implement for divisons routes

--- src/app/modules/divisions/divisions.routes.ts
+++ src/app/modules/divisions/divisions.routes.ts
@@ -1,7 +1,12 @@
 import { Router } from "express";
+import { divisionsController } from "./divisions.controller";
+import { validateRequest } from "../../middleware/validateRequest";
+import { createDivisionSchema } from "./divisions.validations";
+import { verifyToken } from "../../util/verifyToken";
+import { role } from "../user/user.interface";
 
 const router = Router();
-
+router.post("/create", verifyToken(role.ADMIN, role.SUPER_ADMIN), validateRequest(createDivisionSchema), divisionsController.createDivisions);
 
 
 
--- src/app/modules/divisions/divisions.service.ts
+++ src/app/modules/divisions/divisions.service.ts
@@ -2,12 +2,21 @@ import { IDivision } from "./division.interface";
 import { Division } from "./division.model";
 
 const createDivisons = async (payload: IDivision) => {
+  const BaseSlug = payload.name.toLowerCase().split(" ").join("-");
+  let slug = `${BaseSlug}-division`;
   const existingDivision = await Division.findOne({
     where: { name: payload.name },
   });
   if (existingDivision) {
     throw new Error("Division already exists");
   }
+  let count = 0;
+  while (await Division.exists({ slug })) {
+    count++;
+    slug = `${BaseSlug}-division-${count}`;
+  }
+  payload.slug = slug;
+  payload.isActive = true;
   const newDivision = Division.create(payload);
   return newDivision;
 };
