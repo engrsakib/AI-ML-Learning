83c5a87d8f76ea7f55b680d65b2586dec4ce98fa
engrsakib <snazmussakib01@gmail.com>
2025-07-27 20:46:27 +0600

    pyament inteface ready:

---
+++ src/app/modules/payments/payment.interface.ts
@@ -0,0 +1,19 @@
+import { Types } from "mongoose";
+
+export enum PaymentStatus {
+  PAID = "paid",
+  PENDING = "pending",
+  FAILED = "failed",
+  REFUNDED = "refunded",
+}
+
+export interface IPayment {
+  bookingId: Types.ObjectId;
+  transactionId: string;
+  amount: number;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  paymentGetway?: any;
+  invoiceUrl?: string;
+  status: PaymentStatus;
+  createdAt: Date;
+}
