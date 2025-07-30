fca77b0f8b646234981da335fbf5473f9cc63c59
engrsakib <snazmussakib01@gmail.com>
2025-07-30 16:17:08 +0600

    booking update

--- src/app/modules/booking/booking.service.ts
+++ src/app/modules/booking/booking.service.ts
@@ -1,14 +1,36 @@
+import AppError from "../../errorHelpers/appError";
 import getTransactionId from "../../util/getTransctionId";
 import { User } from "../user/user.model";
-import { IBooking } from "./booking.interface";
+import { BookingStatus, IBooking } from "./booking.interface";
+import httpStatus from "http-status-codes";
+import { Booking } from "./booking.model";
 
 const createBooking = async (payload: Partial<IBooking>, userId: string) => {
   const transactionId = getTransactionId();
-  const user = User.findById(userId);
-  if (!user) {
-    throw new Error("User not found");
-  }
+  try {
+    
+    const user = await User.findById(userId);
+    if (!user || !user.phone || !user.address) {
+      throw new AppError(
+        "User must have a phone number and address to create a booking.",
+        httpStatus.BAD_REQUEST,
+      );
+    }
 
+    const booking = await Booking.create({
+      ...payload,
+      user: userId,
+      status: BookingStatus.PENDING,
+    });
+
+    if (!booking) {
+      throw new AppError("Booking creation failed.", httpStatus.INTERNAL_SERVER_ERROR);
+    }
+    
+
+  } catch (error) {
+    throw new AppError(`Error creating booking. ${error}`, httpStatus.INTERNAL_SERVER_ERROR);
+  }
 };
 
 export const bookingService = { createBooking };
