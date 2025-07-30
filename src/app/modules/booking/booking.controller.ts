0ee0dbbdbe15154001fd99caa62953e54c6b3f12
engrsakib <snazmussakib01@gmail.com>
2025-07-30 20:13:29 +0600

    decoded token init

--- src/app/modules/booking/booking.controller.ts
+++ src/app/modules/booking/booking.controller.ts
@@ -2,12 +2,15 @@ import { Request, Response } from "express";
 import AppError from "../../errorHelpers/appError";
 import httpStatus from "http-status-codes";
 import { bookingService } from "./booking.service";
+import { decodedToken } from "../../util/decodedToken";
+
 
 
 const createBooking = async (req: Request, res: Response) => {
   try {
     const token = req.headers.authorization;
-    const booking = await bookingService.createBooking(req.body, "ss");
+    const isTokenValid = decodedToken(token as string);
+    const booking = await bookingService.createBooking(req.body, isTokenValid.userId);
     res.status(200).json({
       message: "Booking created successfully",
       data: booking,
@@ -15,7 +18,7 @@ const createBooking = async (req: Request, res: Response) => {
   } catch (error) {
     throw new AppError(
       `Error creating booking. ${error}`,
-      httpStatus.INTERNAL_SERVER_ERROR
+      httpStatus.INTERNAL_SERVER_ERROR,
     );
   }
 };
