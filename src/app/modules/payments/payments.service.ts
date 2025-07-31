3e01baad4841af3e1d88cd6d382fda8ec193315b
engrsakib <snazmussakib01@gmail.com>
2025-08-01 02:08:23 +0600

    pyaments filed service layr update

--- src/app/modules/payments/payments.service.ts
+++ src/app/modules/payments/payments.service.ts
@@ -36,10 +36,43 @@ const successPayment = async (query: Record<string, string>) => {
 };
 
 
+const failedPayment = async (query: Record<string, string>) => {
+  const session = await Booking.startSession();
+  session.startTransaction();
+  try {
+    const UpdatedPayments = await Payment.findOneAndUpdate(
+      { transactionId: query.transactionId },
+      { status: PaymentStatus.FAILED },
+      { new: true, session },
+    );
+
+    if (!UpdatedPayments) {
+      throw new Error("Payment not found or already updated.");
+    }
+    const updatedBooking = await Booking.findByIdAndUpdate(
+      { _id: UpdatedPayments.bookingId },
+      { status: BookingStatus.FAILED },
+      { new: true, session },
+    );
+
+    if (!updatedBooking) {
+      throw new Error("Booking not found or already updated.");
+    }
+
+    await session.commitTransaction();
+    session.endSession();
+
+    return { success: true, message: "Payment and booking updated successfully." };
+  } catch (error) {
+    throw new Error("Payment success handling failed: " + error);
+  }
+};
+
 
 
 
 
 export const paymentsService = {
   successPayment,
+  failedPayment,
 };
