ec71d402fe3bd0e830e89d9860ace0161436ce69
engrsakib <snazmussakib01@gmail.com>
2025-07-31 17:40:03 +0600

    transaction id creation util function ready

--- src/app/modules/booking/booking.service.ts
+++ src/app/modules/booking/booking.service.ts
@@ -8,70 +8,85 @@ import { Payment } from "../payments/payment.model";
 import { PaymentStatus } from "../payments/payment.interface";
 import { Tour } from "../tour/tour.mode";
 
+
 const createBooking = async (payload: Partial<IBooking>, userId: string) => {
-  const transactionId = getTransactionId();
   try {
-    const user = await User.findById(userId);
-    if (!user || !user.phone || !user.address) {
-      throw new AppError(
-        "User must have a phone number and address to create a booking.",
-        httpStatus.BAD_REQUEST,
-      );
+    const transactionId = getTransactionId();
+    const session = await Booking.startSession();
+    session.startTransaction();
+
+    try {
+      const user = await User.findById(userId, { session });
+      if (!user || !user.phone || !user.address) {
+        throw new AppError(
+          "User must have a phone number and address to create a booking.",
+          httpStatus.BAD_REQUEST,
+        );
+      }
+
+      const tour = await Tour.findById(payload.tour, { session }).select("costFrom");
+      if (!tour?.costFrom) {
+        throw new AppError(
+          "Tour must have a cost to create a booking.",
+          httpStatus.BAD_REQUEST,
+        );
+      }
+
+      const amount = Number(tour.costFrom) * Number(payload.guestCount);
+
+      const booking = await Booking.create({
+        ...payload,
+        user: userId,
+        status: BookingStatus.PENDING,
+      }, { session });
+
+      if (!booking) {
+        throw new AppError(
+          "Booking creation failed.",
+          httpStatus.INTERNAL_SERVER_ERROR,
+        );
+      }
+
+      const payment = await Payment.create({
+        bookingId: booking[0]._id, //when we added session, it returns an array for creation and update, we take the first element
+        status: PaymentStatus.UNPAID,
+        transactionId: transactionId,
+        amount: amount,
+      }, { session });
+
+      if (!payment) {
+        throw new AppError(
+          "Payment creation failed.",
+          httpStatus.INTERNAL_SERVER_ERROR,
+        );
+      }
+
+      const updatedBooking = await Booking.findByIdAndUpdate(
+        booking[0]._id,
+        {
+          payment: payment[0]._id,
+        },
+        { new: true, runValidators: true, session },
+      )
+        .populate("user", "name email phone address")
+        .populate("tour", "name maxGests")
+        .populate("payment", "amount status");
+
+      if (!updatedBooking) {
+        throw new AppError(
+          "Booking update failed.",
+          httpStatus.INTERNAL_SERVER_ERROR,
+        );
+      }
+      await session.commitTransaction();
+      session.endSession();
+      return updatedBooking;
+    } catch (error) {
+      // If any error occurs, we abort the transaction
+      await session.abortTransaction();
+      session.endSession();
+      throw error;
     }
-
-    const tour = await Tour.findById(payload.tour).select("costFrom");
-    if( !tour?.costFrom ) {
-      throw new AppError(
-        "Tour must have a cost to create a booking.",
-        httpStatus.BAD_REQUEST,
-      );
-    }
-
-   
-    const amount = Number(tour.costFrom) * Number(payload.guestCount);
-
-
-    const booking = await Booking.create({
-      ...payload,
-      user: userId,
-      status: BookingStatus.PENDING,
-    });
-
-    if (!booking) {
-      throw new AppError(
-        "Booking creation failed.",
-        httpStatus.INTERNAL_SERVER_ERROR,
-      );
-    }
-
-    
-    const payment = await Payment.create({
-      bookingId: booking._id,
-      status: PaymentStatus.UNPAID,
-      transactionId: transactionId,
-      amount: amount,
-    });
-
-    if (!payment) {
-      throw new AppError(
-        "Payment creation failed.",
-        httpStatus.INTERNAL_SERVER_ERROR,
-      );
-    }
-
-    const updatedBooking = await Booking.findByIdAndUpdate(
-      booking._id,{
-        payment: payment._id,
-      },{ new: true, runValidators: true }).populate("user", "name email phone address").populate("tour","name maxGests").populate("payment", "amount status");
-
-    if (!updatedBooking) {
-      throw new AppError(
-        "Booking update failed.",
-        httpStatus.INTERNAL_SERVER_ERROR,
-      );
-    }
-
-    return updatedBooking;
   } catch (error) {
     throw new AppError(
       `Error creating booking. ${error}`,
