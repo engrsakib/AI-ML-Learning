46b6fcfbd976a82ebd6751d51b3882263a3fd2f9
engrsakib <snazmussakib01@gmail.com>
2025-10-07 00:28:51 +0600

    code sans setup ready

--- backend/prisma/migrations/20250929182735_clcik_top_project/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."Project" ADD COLUMN     "clickCount" INTEGER NOT NULL DEFAULT 0;
--- backend/src/modules/project/project.interface.ts
+++
@@ -1,7 +0,0 @@
- export interface IProjectPagination {
-  page: number
-  limit: number
-  search?: string
-  authorId?: number
-  features?: string[]
-}
